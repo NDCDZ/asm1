daa303cf250bfdfb52591e8e27a9c3fe42469013
dda20102004 <chung2k4ndc@gmail.com>
2024-07-19 22:09:47 +0700

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,319 @@
+@extends('layouts.master')
+@section('title')
+Trang chủ
+@endsection
+@section('content')
+
+<div class="container-fluid paddding mb-5">
+<!-- content -->
+    <div class="row mx-0">
+        <div class="col-md-6 col-12 paddding animate-box" data-animate-effect="fadeIn">
+            <div class="fh5co_suceefh5co_height"><img src="24-news/images/anh1.jpg" alt="img"/>
+                <div class="fh5co_suceefh5co_height_position_absolute"></div>
+                <div class="fh5co_suceefh5co_height_position_absolute_font">
+                    <div class=""><a href="#" class="color_fff"> <i class="fa fa-clock-o"></i>&nbsp;&nbsp;Dec 31,2017
+                    </a></div>
+                    <div class=""><a href="single.html" class="fh5co_good_font"> Sau khi có database mới bổ sung </a></div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="row">
+                <div class="col-md-6 col-6 paddding animate-box" data-animate-effect="fadeIn">
+                    <div class="fh5co_suceefh5co_height_2"><img src="24-news/images/anh2.png" alt="img"/>
+                        <div class="fh5co_suceefh5co_height_position_absolute"></div>
+                        <div class="fh5co_suceefh5co_height_position_absolute_font_2">
+                            <div class=""><a href="#" class="color_fff"> <i class="fa fa-clock-o"></i>&nbsp;&nbsp;Oct
+                                28,2017 </a></div>
+                            <div class=""><a href="single.html" class="fh5co_good_font_2"> Sau khi có database mới bổ sung <br>Sau khi có database mới bổ sung </a></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-6 paddding animate-box" data-animate-effect="fadeIn">
+                    <div class="fh5co_suceefh5co_height_2"><img src="24-news/images/anh3.webp" alt="img"/>
+                        <div class="fh5co_suceefh5co_height_position_absolute"></div>
+                        <div class="fh5co_suceefh5co_height_position_absolute_font_2">
+                            <div class=""><a href="#" class="color_fff"> <i class="fa fa-clock-o"></i>&nbsp;&nbsp;Oct 28,2017 </a></div>
+                            <div class=""><a href="single.html" class="fh5co_good_font_2"> Sau khi có database mới bổ sung </a></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-6 paddding animate-box" data-animate-effect="fadeIn">
+                    <div class="fh5co_suceefh5co_height_2"><img src="24-news/images/anh4.jpg" alt="img"/>
+                        <div class="fh5co_suceefh5co_height_position_absolute"></div>
+                        <div class="fh5co_suceefh5co_height_position_absolute_font_2">
+                            <div class=""><a href="#" class="color_fff"> <i class="fa fa-clock-o"></i>&nbsp;&nbsp;Oct
+                                28,2017 </a></div>
+                            <div class=""><a href="single.html" class="fh5co_good_font_2"> Sau khi có database mới bổ sung </a></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-6 paddding animate-box" data-animate-effect="fadeIn">
+                    <div class="fh5co_suceefh5co_height_2"><img src="24-news/images/anh5.jpg" alt="img"/>
+                        <div class="fh5co_suceefh5co_height_position_absolute"></div>
+                        <div class="fh5co_suceefh5co_height_position_absolute_font_2">
+                            <div class=""><a href="#" class="color_fff"> <i class="fa fa-clock-o"></i>&nbsp;&nbsp;Oct
+                                28,2017 </a></div>
+                            <div class=""><a href="single.html" class="fh5co_good_font_2"> Sau khi có database mới bổ sung </a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container-fluid pt-3">
+<!-- content -->
+    <div class="container animate-box" data-animate-effect="fadeIn">
+        <div>
+            <div class="fh5co_heading fh5co_heading_border_bottom py-2 mb-4">Trending</div>
+        </div>
+        <div class="owl-carousel owl-theme js" id="slider1">
+            <div class="item px-2">
+                <div class="fh5co_latest_trading_img_position_relative">
+                    <div class="fh5co_latest_trading_img"><img src="24-news/images/anh6.jpg" alt=""
+                    class="fh5co_img_special_relative"/></div>
+                    <div class="fh5co_latest_trading_img_position_absolute"></div>
+                    <div class="fh5co_latest_trading_img_position_absolute_1">
+                        <a href="single.html" class="text-white"> Sau khi có database mới bổ sung </a>
+                        <div class="fh5co_latest_trading_date_and_name_color"> Walter Johson - March 7,2017</div>
+                    </div>
+                </div>
+            </div>
+            <div class="item px-2">
+                <div class="fh5co_latest_trading_img_position_relative">
+                    <div class="fh5co_latest_trading_img"><img src="24-news/images/anh7.jpg" alt="" class="fh5co_img_special_relative"/></div>
+                    <div class="fh5co_latest_trading_img_position_absolute"></div>
+                    <div class="fh5co_latest_trading_img_position_absolute_1">
+                        <a href="single.html" class="text-white"> Sau khi có database mới bổ sung </a>
+                        <div class="fh5co_latest_trading_date_and_name_color"> Walter Johson - March 7,2017</div>
+                    </div>
+                </div>
+            </div>
+            <div class="item px-2">
+                <div class="fh5co_latest_trading_img_position_relative">
+                    <div class="fh5co_latest_trading_img"><img src="24-news/images/anh8.jpg" alt="" class="fh5co_img_special_relative"/></div>
+                    <div class="fh5co_latest_trading_img_position_absolute"></div>
+                    <div class="fh5co_latest_trading_img_position_absolute_1">
+                        <a href="single.html" class="text-white"> Sau khi có database mới bổ sung </a>
+                        <div class="fh5co_latest_trading_date_and_name_color"> Walter Johson - March 7,2017</div>
+                    </div>
+                </div>
+            </div>
+            <div class="item px-2">
+                <div class="fh5co_latest_trading_img_position_relative">
+                    <div class="fh5co_latest_trading_img"><img src="24-news/images/anh9.jpg" alt="" class="fh5co_img_special_relative"/></div>
+                    <div class="fh5co_latest_trading_img_position_absolute"></div>
+                    <div class="fh5co_latest_trading_img_position_absolute_1">
+                        <a href="single.html" class="text-white"> Sau khi có database mới bổ sung </a>
+                        <div class="fh5co_latest_trading_date_and_name_color"> Walter Johson - March 7,2017</div>
+                    </div>
+                </div>
+            </div>
+            <div class="item px-2">
+                <div class="fh5co_latest_trading_img_position_relative">
+                    <div class="fh5co_latest_trading_img"><img src="24-news/images/anh10.jpg" alt="" class="fh5co_img_special_relative"/></div>
+                    <div class="fh5co_latest_trading_img_position_absolute"></div>
+                    <div class="fh5co_latest_trading_img_position_absolute_1">
+                        <a href="single.html" class="text-white"> Sau khi có database mới bổ sung </a>
+                        <div class="fh5co_latest_trading_date_and_name_color"> Walter Johson - March 7,2017</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container-fluid pb-4 pt-5">
+<!-- content -->
+    <div class="container animate-box">
+        <div>
+            <div class="fh5co_heading fh5co_heading_border_bottom py-2 mb-4">News</div>
+        </div>
+        <div class="owl-carousel owl-theme" id="slider2">
+            <div class="item px-2">
+                <div class="fh5co_hover_news_img">
+                    <div class="fh5co_news_img"><img src="24-news/images/anh11.jpg" alt=""/></div>
+                    <div>
+                        <a href="single.html" class="d-block fh5co_small_post_heading"><span class="">Sau khi có database mới bổ sung</span></a>
+                        <div class="c_g"><i class="fa fa-clock-o"></i> Oct 16,2017</div>
+                    </div>
+                </div>
+            </div>
+            <div class="item px-2">
+                <div class="fh5co_hover_news_img">
+                    <div class="fh5co_news_img"><img src="24-news/images/anh12.jpg" alt=""/></div>
+                    <div>
+                        <a href="single.html" class="d-block fh5co_small_post_heading"><span class="">Sau khi có database mới bổ sung</span></a>
+                        <div class="c_g"><i class="fa fa-clock-o"></i> Oct 16,2017</div>
+                    </div>
+                </div>
+            </div>
+            <div class="item px-2">
+                <div class="fh5co_hover_news_img">
+                    <div class="fh5co_news_img"><img src="24-news/images/anh13.jpg" alt=""/></div>
+                    <div>
+                        <a href="single.html" class="d-block fh5co_small_post_heading"><span class="">Sau khi có database mới bổ sung</span></a>
+                        <div class="c_g"><i class="fa fa-clock-o"></i> Oct 16,2017</div>
+                    </div>
+                </div>
+            </div>
+            <div class="item px-2">
+                <div class="fh5co_hover_news_img">
+                    <div class="fh5co_news_img"><img src="24-news/images/anh14.jpg" alt=""/></div>
+                    <div>
+                        <a href="single.html" class="d-block fh5co_small_post_heading"><span class="">Sau khi có database mới bổ sung</span></a>
+                        <div class="c_g"><i class="fa fa-clock-o"></i> Oct 16,2017</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container-fluid fh5co_video_news_bg pb-4">
+<!-- content -->
+</div>
+<div class="container-fluid pb-4 pt-4 paddding">
+<!-- content -->
+    <div class="container paddding">
+        <div class="row mx-0">
+            <div class="col-md-8 animate-box" data-animate-effect="fadeInLeft">
+                <div>
+                    <div class="fh5co_heading fh5co_heading_border_bottom py-2 mb-4">News</div>
+                </div>
+                <div class="row pb-4">
+                    <div class="col-md-5">
+                        <div class="fh5co_hover_news_img">
+                            <div class="fh5co_news_img"><img src="24-news/images/anh10.jpg" alt=""/></div>
+                            <div></div>
+                        </div>
+                    </div>
+                    <div class="col-md-7 animate-box">
+                        <a href="single.html" class="fh5co_magna py-2"> Sau khi có database mới bổ sung </a> <a href="single.html" class="fh5co_mini_time py-3"> Thomson Smith -
+                        April 18,2016 </a>
+                        <div class="fh5co_consectetur"> Sau khi có database mới bổ sung
+                        </div>
+                    </div>
+                </div>
+                <div class="row pb-4">
+                    <div class="col-md-5">
+                        <div class="fh5co_hover_news_img">
+                            <div class="fh5co_news_img"><img src="24-news/images/anh15.jpg" alt=""/></div>
+                            <div></div>
+                        </div>
+                    </div>
+                    <div class="col-md-7">
+                        <a href="single.html" class="fh5co_magna py-2"> Sau khi có database mới bổ sung </a> <a href="#" class="fh5co_mini_time py-3"> Thomson Smith -
+                        April 18,2016 </a>
+                        <div class="fh5co_consectetur"> Sau khi có database mới bổ sung
+                        </div>
+                        <ul class="fh5co_gaming_topikk pt-3">
+                            <li> Why 2017 Might Just Be the Worst Year Ever for Gaming</li>
+                            <li> Ghost Racer Wants to Be the Most Ambitious Car Game</li>
+                            <li> New Nintendo Wii Console Goes on Sale in Strategy Reboot</li>
+                            <li> You and Your Kids can Enjoy this News Gaming Console</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row pb-4">
+                    <div class="col-md-5">
+                        <div class="fh5co_hover_news_img">
+                            <div class="fh5co_news_img">
+                                <img src="24-news/images/anh16.jpg" alt=""/>
+                            </div>
+                            <div></div>
+                        </div>
+                    </div>
+                    <div class="col-md-7">
+                        <a href="single.html" class="fh5co_magna py-2"> Sau khi có database mới bổ sung </a> <a href="#" class="fh5co_mini_time py-3"> Thomson Smith -
+                        April 18,2016 </a>
+                        <div class="fh5co_consectetur">Sau khi có database mới bổ sung
+                        </div>
+                    </div>
+                </div>
+                <div class="row pb-4">
+                    <div class="col-md-5">
+                        <div class="fh5co_hover_news_img">
+                            <div class="fh5co_news_img"><img src="24-news/images/anh17.jpg" alt=""/></div>
+                            <div></div>
+                        </div>
+                    </div>
+                    <div class="col-md-7">
+                        <a href="single.html" class="fh5co_magna py-2">Sau khi có database mới bổ sung </a> <a href="#" class="fh5co_mini_time py-3"> Thomson Smith -
+                        April 18,2016 </a>
+                        <div class="fh5co_consectetur"> Sau khi có database mới bổ sung
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 animate-box" data-animate-effect="fadeInRight">
+                <div>
+                    <div class="fh5co_heading fh5co_heading_border_bottom py-2 mb-4">Tags</div>
+                </div>
+                <div class="clearfix"></div>
+                <div class="fh5co_tags_all">
+                    <a href="#" class="fh5co_tagg">Business</a>
+                    <a href="#" class="fh5co_tagg">Technology</a>
+                    <a href="#" class="fh5co_tagg">Sport</a>
+                    <a href="#" class="fh5co_tagg">Art</a>
+                    <a href="#" class="fh5co_tagg">Lifestyle</a>
+                    <a href="#" class="fh5co_tagg">Three</a>
+                    <a href="#" class="fh5co_tagg">Photography</a>
+                    <a href="#" class="fh5co_tagg">Lifestyle</a>
+                    <a href="#" class="fh5co_tagg">Art</a>
+                    <a href="#" class="fh5co_tagg">Education</a>
+                    <a href="#" class="fh5co_tagg">Social</a>
+                    <a href="#" class="fh5co_tagg">Three</a>
+                </div>
+                <div>
+                    <div class="fh5co_heading fh5co_heading_border_bottom pt-3 py-2 mb-4">Most Popular</div>
+                </div>
+                <div class="row pb-3">
+                    <div class="col-5 align-self-center">
+                        <img src="24-news/images/anh18.jpg" alt="img" class="fh5co_most_trading"/>
+                    </div>
+                    <div class="col-7 paddding">
+                        <div class="most_fh5co_treding_font"> Sau khi có database mới bổ sung</div>
+                        <div class="most_fh5co_treding_font_123"> April 18, 2016</div>
+                    </div>
+                </div>
+                <div class="row pb-3">
+                    <div class="col-5 align-self-center">
+                        <img src="24-news/images/anh19.jpg" alt="img" class="fh5co_most_trading"/>
+                    </div>
+                    <div class="col-7 paddding">
+                        <div class="most_fh5co_treding_font"> Sau khi có database mới bổ sung</div>
+                        <div class="most_fh5co_treding_font_123"> April 18, 2016</div>
+                    </div>
+                </div>
+                <div class="row pb-3">
+                    <div class="col-5 align-self-center">
+                        <img src="24-news/images/anh20.jpg" alt="img" class="fh5co_most_trading"/>
+                    </div>
+                    <div class="col-7 paddding">
+                        <div class="most_fh5co_treding_font"> Sau khi có database mới bổ sung</div>
+                        <div class="most_fh5co_treding_font_123"> April 18, 2016</div>
+                    </div>
+                </div>
+                <div class="row pb-3">
+                    <div class="col-5 align-self-center"><img src="24-news/images/anh21.jpg" alt="img"
+                                                              class="fh5co_most_trading"/></div>
+                    <div class="col-7 paddding">
+                        <div class="most_fh5co_treding_font"> Sau khi có database mới bổ sung</div>
+                        <div class="most_fh5co_treding_font_123"> April 18, 2016</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row mx-0 animate-box" data-animate-effect="fadeInUp">
+            <div class="col-12 text-center pb-4 pt-4">
+                <a href="#" class="btn_mange_pagging"><i class="fa fa-long-arrow-left"></i>&nbsp;&nbsp; Previous</a>
+                <a href="#" class="btn_pagging">1</a>
+                <a href="#" class="btn_pagging">2</a>
+                <a href="#" class="btn_pagging">3</a>
+                <a href="#" class="btn_pagging">...</a>
+                <a href="#" class="btn_mange_pagging">Next <i class="fa fa-long-arrow-right"></i>&nbsp;&nbsp; </a>
+             </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/single.blade.php
@@ -0,0 +1,157 @@
+@extends('layouts.master')
+@section('title')
+Trang Single
+@endsection
+@section('content')
+<body class="single">
+
+<div id="fh5co-title-box" style="background-image: url(24-news/images/anh23.jpg); background-position: 50% 90.5px;" data-stellar-background-ratio="0.5">
+    <div class="overlay"></div>
+    <div class="page-title">
+        <img src="24-news/images/anh24.jpg" alt="Free HTML5 by FreeHTMl5.co">
+        <span>January 1, 2018</span>
+        <h2>Champions 2024 Sắp khởi tranh</h2>
+    </div>
+</div>
+<div id="fh5co-single-content" class="container-fluid pb-4 pt-4 paddding">
+    <div class="container paddding">
+        <div class="row mx-0">
+            <div class="col-md-8 animate-box" data-animate-effect="fadeInLeft">
+                <p>
+                    8 đội tuyển, 8 màu sắc thi đấu khác nhau. Sẽ rất khó để dự đoán người chiến thắng vì những đội nữ
+                     xuất hiện tại sự kiện này đều các tập thể xuất sắc nhất tại khu vực của họ. Và sau đây chính là các
+                      đội tuyển Valorant đáng chú ý nhất trong VCT Game Changers Championship 2024.
+                </p>
+                <p>
+                    Shopify Rebellion là đội Game Changers của Canada thi đấu ở khu vực NA. Đội tuyển này có tiền
+                    thân là Version1 cũ và được xếp là hạt giống số 2 khu vực NA nhờ có số điểm tích lũy vượt trội.
+
+
+                </p>
+                <h3>Khởi tranh</h3>
+                <p>
+                    Shopify Rebellion đã hoàn toàn toàn thống trị khu vực của họ. Các cô gái Shopify đã xuất
+                    sắc giành 2 chức vô địch liên tiếp tại Stage I & II, tuy nhiên chuỗi thành tích bất bại chỉ
+                     kéo dài cho đến trận đấu cuối cùng khi họ đã để thua đội GC của Evil Geniuses ở Stage III.
+
+                </p>
+                <ul>
+                    <li>Chao Hui EDward Gaming</li>
+                    <li>Chao Hui EDward Gaming</li>
+                    <li>Chao Hui EDward Gaming</li>
+                    <li>Chao Hui EDward Gaming</li>
+                </ul>
+                <p>
+                    Chao Hui EDward Gaming là một đội Valorant của Trung Quốc đã phải  thi đấu ở Vòng loại VCT khu
+                     vực để lọt vào Game Changers East Asia. Tại đây, các đội GC xuất sắc nhất đến từ Nhật Bản, Hàn
+                      Quốc và Trung Quốc sẽ cạnh tranh lấy một tấm vé duy nhất để đến với Sao Paulo.
+                </p>
+            </div>
+            <div class="col-md-3 animate-box" data-animate-effect="fadeInRight">
+                <div>
+                    <div class="fh5co_heading fh5co_heading_border_bottom py-2 mb-4">Tags</div>
+                </div>
+                <div class="clearfix"></div>
+                <div class="fh5co_tags_all">
+                    <a href="#" class="fh5co_tagg">Business</a>
+                    <a href="#" class="fh5co_tagg">Technology</a>
+                    <a href="#" class="fh5co_tagg">Sport</a>
+                    <a href="#" class="fh5co_tagg">Art</a>
+                    <a href="#" class="fh5co_tagg">Lifestyle</a>
+                    <a href="#" class="fh5co_tagg">Three</a>
+                    <a href="#" class="fh5co_tagg">Photography</a>
+                    <a href="#" class="fh5co_tagg">Lifestyle</a>
+                    <a href="#" class="fh5co_tagg">Art</a>
+                    <a href="#" class="fh5co_tagg">Education</a>
+                    <a href="#" class="fh5co_tagg">Social</a>
+                    <a href="#" class="fh5co_tagg">Three</a>
+                </div>
+                <div>
+                    <div class="fh5co_heading fh5co_heading_border_bottom pt-3 py-2 mb-4">Most Popular</div>
+                </div>
+                <div class="row pb-3">
+                    <div class="col-5 align-self-center">
+                        <img src="24-news/images/anh25.jpg" alt="img" class="fh5co_most_trading"/>
+                    </div>
+                    <div class="col-7 paddding">
+                        <div class="most_fh5co_treding_font"> Magna aliqua ut enim ad minim veniam quis nostrud.</div>
+                        <div class="most_fh5co_treding_font_123"> April 18, 2016</div>
+                    </div>
+                </div>
+                <div class="row pb-3">
+                    <div class="col-5 align-self-center">
+                        <img src="24-news/images/anh26.jpg" alt="img" class="fh5co_most_trading"/>
+                    </div>
+                    <div class="col-7 paddding">
+                        <div class="most_fh5co_treding_font"> Enim ad minim veniam nostrud xercitation ullamco.</div>
+                        <div class="most_fh5co_treding_font_123"> April 18, 2016</div>
+                    </div>
+                </div>
+                <div class="row pb-3">
+                    <div class="col-5 align-self-center">
+                        <img src="24-news/images/anh27.jpg" alt="img" class="fh5co_most_trading"/>
+                    </div>
+                    <div class="col-7 paddding">
+                        <div class="most_fh5co_treding_font"> Magna aliqua ut enim ad minim veniam quis nostrud.</div>
+                        <div class="most_fh5co_treding_font_123"> April 18, 2016</div>
+                    </div>
+                </div>
+                <div class="row pb-3">
+                    <div class="col-5 align-self-center"><img src="24-news/images/anh28.jpg" alt="img"
+                                                              class="fh5co_most_trading"/></div>
+                    <div class="col-7 paddding">
+                        <div class="most_fh5co_treding_font"> Magna aliqua ut enim ad minim veniam quis nostrud.</div>
+                        <div class="most_fh5co_treding_font_123"> April 18, 2016</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container-fluid pb-4 pt-5">
+    <div class="container animate-box">
+        <div>
+            <div class="fh5co_heading fh5co_heading_border_bottom py-2 mb-4">Trending</div>
+        </div>
+        <div class="owl-carousel owl-theme" id="slider2">
+            <div class="item px-2">
+                <div class="fh5co_hover_news_img">
+                    <div class="fh5co_news_img"><img src="24-news/images/anh29.jpg" alt=""/></div>
+                    <div>
+                        <a href="#" class="d-block fh5co_small_post_heading"><span class="">The top 10 best computer speakers in the market</span></a>
+                        <div class="c_g"><i class="fa fa-clock-o"></i> Oct 16,2017</div>
+                    </div>
+                </div>
+            </div>
+            <div class="item px-2">
+                <div class="fh5co_hover_news_img">
+                    <div class="fh5co_news_img"><img src="24-news/images/anh30.jpg" alt=""/></div>
+                    <div>
+                        <a href="#" class="d-block fh5co_small_post_heading"><span class="">The top 10 best computer speakers in the market</span></a>
+                        <div class="c_g"><i class="fa fa-clock-o"></i> Oct 16,2017</div>
+                    </div>
+                </div>
+            </div>
+            <div class="item px-2">
+                <div class="fh5co_hover_news_img">
+                    <div class="fh5co_news_img"><img src="24-news/images/anh31.jpg" alt=""/></div>
+                    <div>
+                        <a href="#" class="d-block fh5co_small_post_heading"><span class="">The top 10 best computer speakers in the market</span></a>
+                        <div class="c_g"><i class="fa fa-clock-o"></i> Oct 16,2017</div>
+                    </div>
+                </div>
+            </div>
+            <div class="item px-2">
+                <div class="fh5co_hover_news_img">
+                    <div class="fh5co_news_img"><img src="24-news/images/anh23.jpg" alt=""/></div>
+                    <div>
+                        <a href="#" class="d-block fh5co_small_post_heading"><span class="">The top 10 best computer speakers in the market</span></a>
+                        <div class="c_g"><i class="fa fa-clock-o"></i> Oct 16,2017</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+@endsection
